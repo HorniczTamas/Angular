563e9fba28a3f01cf4ac5553df881b053fafe379
HorniczTamas <hornicztamas@gmail.com>
2025-11-10 13:57:50 +0100

    Project

---
+++ 01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '';
+  enteredAnnualInvestment = '';
+  enteredExpectedReturn = '';
+  enteredDuration = '';
+
+  onSubmit() {
+    console.log('Form submitted!');
+    console.log('Initial Investment:', this.enteredInitialInvestment);
+    console.log('Annual Investment:', this.enteredAnnualInvestment);
+    console.log('Expected Return:', this.enteredExpectedReturn);
+    console.log('Duration:', this.enteredDuration);
+  }
+}
